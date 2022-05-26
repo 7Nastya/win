a83cfb6c7a75d4dd9eb3214eeed86302925f2084
79373 <saa_1299@mail.ru>
2022-05-26 16:26:49 +0300

    generator and iterator

--- generator_iterator/generator_iterator.py
+++ generator_iterator/generator_iterator.py
@@ -1,5 +1,24 @@
-def squares(start, stop):
-    for i in range(start, stop):
-        yield i * i
+# def even_range(start, end):
+#     current = start
+#     while current < end:
+#         yield current
+#         current += 2
+#
+# ran = even_range(0, 4)
+# x = next(ran)
+# print(x)
+# y = next(ran)
+# print(y)
+# z = next(ran)
+# print(z)
 
-generator = squares(1, 10)
+#Можем проверить, что функция прерывается каждый раз после выполнения yield:
+def list_generator(list_obj):
+    for item in list_obj:
+        yield item
+        print('After yieloding {}'.format(item))
+
+generator = list_generator([1, 2])
+next(generator)
+next(generator)
+next(generator)
