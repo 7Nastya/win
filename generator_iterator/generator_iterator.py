a26f511528462e2c683b17a9a569d28308281aa9
79373 <saa_1299@mail.ru>
2022-05-27 13:25:01 +0300

    generator and iterator

--- generator_iterator/generator_iterator.py
+++ generator_iterator/generator_iterator.py
@@ -13,12 +13,31 @@
 # print(z)
 
 #Можем проверить, что функция прерывается каждый раз после выполнения yield:
-def list_generator(list_obj):
-    for item in list_obj:
-        yield item
-        print('After yieloding {}'.format(item))
+# def list_generator(list_obj):
+#     for item in list_obj:
+#         yield item
+#         print('After yieloding {}'.format(item))
+#
+# generator = list_generator([1, 2])
+# next(generator)
+# next(generator)
+# next(generator)
 
-generator = list_generator([1, 2])
+#генератор accumulator, который
+# хранит общее количество данных и в бесконечном цикле получает с помощью оператора
+# yield значение
+def accumulator():
+    total = 0
+    while True:
+        value = yield total
+        print('Got: {}'.format(value))
+
+        if not value: break
+        total += value
+
+generator = accumulator()
+
 next(generator)
-next(generator)
-next(generator)
+print('Accumulated: {}'.format(generator.send(1)))
+print('Accumulated: {}'.format(generator.send(1)))
+print('Accumulated: {}'.format(generator.send(1)))
