996426a5684bd5fc9dd4315dac03898c2cb37286
Nastya_7 <saa_1299@mail.ru>
2022-06-06 17:51:39 +0300

    Operation minus

--- DIVMOD/divmod.py
+++ DIVMOD/divmod.py
@@ -1,8 +1,8 @@
 class multi:
     def operation(self):
         a = 1
-        a = a + 1
-        a = a + 1
-        a = a + 1
-        a = a + 1
+        a = a + 2
+        a = a + 2
+        a = a + 2
+        a = a + 2
 
