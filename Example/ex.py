cf42b7474da6787becc2a879e2a9d70277c7c416
Nastya_7 <saa_1299@mail.ru>
2022-09-29 14:45:03 +0300

    Удаление дубликатов из списка

--- Example/ex.py
+++ Example/ex.py
@@ -1,3 +1,11 @@
 # a, b = input().split()
 # print(int(a) + int(b))
 # with 'input.txt' as f:
+c = {}
+c[1] = 1
+c['1'] = 2
+c[1] += 1
+s = 0
+for k in c:
+    s += c[k]
+print(s)
--- second_dir/second.py
+++ second_dir/second.py
@@ -1,2 +1,12 @@
-print('Hello, world!')
-print("How?")
+def del_duplicates(duplicate_list):
+    result = []
+    for elem in duplicate_list:
+        if elem not in result:
+            result.append(elem)
+    return result
+
+
+t = [{"key1": "value1"}, {"k1": "v1", "k2": "v2", "k3": "v3"}, {}, {}, {"key1": "value1"}, {"key1": "value1"},
+     {"key2": "value2"}]
+
+print(del_duplicates(t))
